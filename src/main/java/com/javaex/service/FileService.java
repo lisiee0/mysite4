0f939be95cf27766c73912951c33985cd6f7d0b7
Hojun_Yoo <lisiee0@naver.com>
2022-02-04 12:19:58 +0900

    02-04

--- src/main/java/com/javaex/service/FileService.java
+++ src/main/java/com/javaex/service/FileService.java
@@ -1,5 +1,7 @@
 package com.javaex.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,10 +11,19 @@ public class FileService {
 	public void restore(MultipartFile file) {
 		System.out.println("fileservice/restore()");
 		
-		System.out.println(file.getOriginalFilename());
 		
-		// 파일을 하드디스크에 저장
 		
+		// 파일을 하드디스크에 저장(운영내용)
+		
+		// 원본파일이름
+		String orgName= file.getOriginalFilename();
+		
+		// 확장자
+		String exName= orgName.substring(orgName.lastIndexOf("."));
+
+		// 저장파일이름
+		String saveName= System.currentTimeMillis()+UUID.randomUUID().toString()+exName;
+		System.out.println(saveName);
 		
 		
 		
