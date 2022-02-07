c29e40ff6bad3d0d1de65954a60385e120560244
Hojun_Yoo <lisiee0@naver.com>
2022-02-07 15:34:51 +0900

    paging

---
+++ sql.savefile/mysite4_board_paging.sql
@@ -0,0 +1,30 @@
+select  rn,
+        no,
+        title,
+        content,
+        hit,
+        regDate,
+        userNo,
+        userName
+from (select  rownum rn,
+              no,
+              title,
+              content,
+              hit,
+              regDate,
+              userNo,
+              userName
+     from    (select  b.no no,
+                      title,
+                      content,
+                      hit,
+                      to_char(reg_date, 'YYYY-MM-DD HH24:MI:SS') regDate,
+                      u.no userNo,
+                      u.name userName
+             from     board b, users u
+             where    b.user_no= u.no
+             order by b.no desc) ot) rt
+where   rn between 1 and 10;
+
+
+
